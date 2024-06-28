226ed73b5b4dc1817d0475c041da47967c63cf65
SaiHarikaGarapati <71425722+SaiHarikaGarapati@users.noreply.github.com>
2024-06-28 15:03:05 +0530

    Add files via upload
    Length Of Last String from LeetCode

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        s = s.Trim();
+        int count = 0;
+        for (int i = s.Length-1;i >= 0;i--)
+        {
+            if(s[i] == ' ')
+            {
+                break;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+public class Last
+{
+    public static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        Console.WriteLine(s.LengthOfLastWord("Hello World  "));
+        Console.ReadKey();
+    }
+}
